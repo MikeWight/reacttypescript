6eba1f97fbfd8d2225cf9f9d7ad04eb6fad03ffa
MikeWight <michel.wight@hotmail.com>
2022-10-14 22:06:52 -0300

    added setStates

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./styles.css";
-const InputField = () => {
+
+interface Props {
+	todo: string;
+	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
 	return (
-		<form className="input">
+		<form className="input" onSubmit={handleAdd}>
 			<input
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
 				type="input"
 				placeholder="Enter a task"
 				className="input__box"
